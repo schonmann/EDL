27e2c572ddf514bd819f071a395d3f98191d6808
antonio_schonmann@hotmail.com <xumatt321>
2016-09-04 03:57:58 -0300

    Game title added.

--- trabalhos/trabalho-02/main.lua
+++ trabalhos/trabalho-02/main.lua
@@ -5,6 +5,8 @@ local renderer = require("renderer.renderer")
 
 function love.load()
     love.window.setMode(constants.VIEWPORT_WIDTH, constants.VIEWPORT_HEIGHT)
+    love.window.setTitle(constants.GAME_TITLE)
+    
     love.graphics.setDefaultFilter('nearest')
 
     playerObject = Player:new(nil, love.graphics.newImage(assets.PATH_IMG_PLAYER))
--- trabalhos/trabalho-02/util/constants.lua
+++ trabalhos/trabalho-02/util/constants.lua
@@ -2,5 +2,6 @@ local constants = {}
 
 constants.VIEWPORT_WIDTH = 800
 constants.VIEWPORT_HEIGHT = 600
+constants.GAME_TITLE = "EDL - Trabalho 02"
 
 return constants
